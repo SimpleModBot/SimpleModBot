94482f3d3ce8bbc8b4fdaeaf50658c8f7ec039e1
DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
2021-02-19 13:07:58 -0800

    Updated HelpCommand
    Fixed some spelling errors within the help command.

--- src/commands/information/HelpCommand.js
+++ src/commands/information/HelpCommand.js
@@ -10,15 +10,15 @@ module.exports = class HelpCommand extends BaseCommand {
     const sectionEmbed = new Discord.MessageEmbed()
    .setTitle('Bot Help Sections')
    .setDescription('Use //help sectionName to access another section.\nSections:\ninformation\nfun\nmoderation')
-   .addField('Fun Commands \`fun\`', 'Commands that all users can user that are for fun and have no purpose.')
-   .addField('Information commands \` information\`', 'Commands that return some form of important imformation.')
-   .addField('Moderation commands \`moderation\`', 'Commands that are for moderation purposes within a server.')
+   .addField('Fun Commands', 'Commands that all users can use that are for fun and have no purpose.')
+   .addField('Information commands', 'Commands that return some information.')
+   .addField('Moderation commands', 'Commands that are for moderation within a server.')
    .setFooter(client.user.tag, client.user.displayAvatarURL());
  
 const infoEmbed = new Discord.MessageEmbed()  
    .setTitle('Information Commands.')
    .addField('Help Commands', 'This commands shows the user all the commands possable.')
-   .addField('Social Command', 'Displays social media in an embed.');
+   .addField('Social Command', 'Displays social media in an embed and bot invite.');
  
 const funEmbed = new Discord.MessageEmbed()
    .setTitle('Fun Commands.')
@@ -31,12 +31,12 @@ const moderationEmbed = new Discord.MessageEmbed()
    .setTitle('Moderation Commands.')
    .addField('Ban Command', 'Bans a member from the server')
    .addField('Kick Command', 'Kicks a member from the server')
-   .addField('Mute Command', 'Mutes a member in the server')
+   .addField('Mute Command', 'Mutes a member in the server \`wip\`')
    .addField('Nuke Command', 'Clones a channel and deletes the old one.')
    .addField('Purge Command', 'Purges messages within a channel')
-   .addField('Recruit Command', 'Recruits a member to the staff team in a server')
+   .addField('Recruit Command', 'Recruits a member to the staff team in a server \`wip\`')
    .addField('Unban Command', 'Unbans a member from the server')
-   .addField('Unmute Command', 'Unmutes a member in a server');
+   .addField('Unmute Command', 'Unmutes a member in a server \`wip\`');
  
 if (!args[0]) return message.channel.send(sectionEmbed);
 if (args[0] == 'information') return message.channel.send(infoEmbed);
