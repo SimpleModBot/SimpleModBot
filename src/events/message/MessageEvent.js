63a69256f4b55881445101002530eeb1b2cfb450
DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
2021-02-21 16:53:53 -0800

    reverted from bugs

--- src/events/message/MessageEvent.js
+++ src/events/message/MessageEvent.js
@@ -4,27 +4,18 @@ module.exports = class MessageEvent extends BaseEvent {
   constructor() {
     super('message');
   }
-  
+
   async run(client, message) {
     if (message.author.bot) return;
     if (message.content.startsWith(client.prefix)) {
       const [cmdName, ...cmdArgs] = message.content
-      .slice(client.prefix.length)
-      .trim()
-      .split(/\s+/);
+        .slice(client.prefix.length)
+        .trim()
+        .split(/\s+/);
       const command = client.commands.get(cmdName);
       if (command) {
         command.run(client, message, cmdArgs);
       }
     }
   }
-  async run(client) {
-    let serverIn = await client.guilds.cache.size;
-    console.log(client.user.tag + ' has logged in.');
-    client.user.setPresence({
-      activity: {
-        name: `${serverIn} servers. Prefix: //`,
-        type: "WATCHING"
-      }, status: 'online'
-    })}
 }
