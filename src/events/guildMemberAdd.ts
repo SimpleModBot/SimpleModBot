a2cef06f3431275566d21eebb56b3d447fbb3c99
DEATHB4DEFEAT <zachcaffee@outlook.com>
2022-01-28 10:18:14 -0800

    dev dependency removal

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -6,20 +6,30 @@ const mongoose = require('mongoose');
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member, client) {
-        {
-            let guildProfile = await schema.findOne({ guildID: member.guild.id });
-            if (!guildProfile) return;
-            if (!guildProfile.welcomeChannelID) return;
-            const welcomechannel = await member.guild.channels.cache.get(guildProfile.welcomeChannelID);
-            if (!welcomechannel) return;
+        let guildProfile = await schema.findOne({ guildID: member.guild.id });
+        if (!guildProfile) return;
+        if (!guildProfile.welcomeChannelID) return;
+        const welcomechannel = await member.guild.channels.cache.get(
+            guildProfile.welcomeChannelID,
+        );
+        if (!welcomechannel) return;
 
-            const welcomeEmbed = new Discord.MessageEmbed()
-                .setTitle("A new user has joined!")
-                .setDescription(`Everyone welcome ${member.user.tag}!`)
-                .setTimestamp()
-                .setColor("GREY");
+        const welcomeEmbed = new Discord.MessageEmbed()
+            .setTitle('A new user has joined!')
+            .setDescription(
+                `Everyone welcome ${member.user.tag}!\nIf the server is set up with the verification system you can probably do \`${client.prefix}verify\` to gain access to the server.`,
+            )
+            .setTimestamp()
+            .setColor('GREY');
 
-            if (welcomechannel.permissionsFor(member.guild.me).has(['SEND_MESSAGES'])) welcomechannel.send({ content: "Welcome!", embeds: [welcomeEmbed] });
-        }
+        if (
+            welcomechannel
+                .permissionsFor(member.guild.me)
+                .has(['SEND_MESSAGES'])
+        )
+            welcomechannel.send({
+                content: 'Welcome!',
+                embeds: [welcomeEmbed],
+            });
     },
 };
