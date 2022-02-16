a6c98485d696d1b4021501b54946ff04b4bac880
DEATHB4DEFEAT <zachcaffee@outlook.com>
2022-02-15 18:47:19 -0800

    remove old prefix system from database

--- src/database/models/guildSchema.ts
+++ src/database/models/guildSchema.ts
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const guildSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     guildID: String,
-    prefix: { type: String, default: "\\" },
     prefixes: { type: Array, default: [] },
     memberRoleID: { type: String, required: false },
     modlogChannelID: { type: String, required: false },
--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -27,7 +27,6 @@ module.exports = {
 		client.prefix = '\\';
 		client.prefixes = [`<@!?911112976793215006>`, `smb`, `hey smb`, `oi smb`, `simplemodbot`, `hey simplemodbot`, `oi simplemodbot`, `shithead`, `shitbot`, `shit bot`];
 
-		if (guildProfile.prefix) client.prefix = guildProfile.prefix;
 		if (guildProfile.prefixes) {
 			if (!guildProfile.prefixes) return;
 			guildProfile.prefixes.forEach((prefix) => {
