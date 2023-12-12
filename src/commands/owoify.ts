fa00eb0ddb3344c9029722eb83346491740dff80
Stanford47 <scoding@mailfence.com>
2023-12-12 09:34:30 -0800

    owo
    - made owoify command (completely untested lol)
    
    - removed package: @zuzak/owo (not needed anymore)
    
    - fixed tsconfig

---
+++ src/commands/owoify.ts
@@ -0,0 +1,24 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import OwO from "../utils/owoifyFunc";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("owoify")
+        .setDescription("Makes your text silly :3")
+        .addStringOption(o => o
+            .setName("text")
+            .setDescription("text to owoify")
+            .setRequired(true)),
+    
+    exec(interaction: ChatInputCommandInteraction) {
+        const getString = interaction.options.getString("text");
+
+        interaction.reply({
+            ephemeral: false,
+            embeds: [new EmbedBuilder()
+                        .setTitle(new OwO().translateText("Here\'s your translated text :3"))
+                        .setDescription(`\`${new OwO().translateText(getString as string)}`)
+                    ]
+        })
+    }
+}
---
+++ src/utils/owoifyFunc.ts
@@ -0,0 +1,85 @@
+class OwO {
+    private readonly prefixes: Array<string> = [
+        "<3 ",
+        "0w0 ",
+        "H-hewwo?? ",
+        "HIIII! ",
+        "Haiiii! ",
+        "Huohhhh. ",
+        "OWO ",
+        "OwO ",
+        "UwU "
+    ];
+    private readonly suffixes: Array<string> = [
+        " ( ´•̥̥̥ω•̥̥̥` )",
+        " ( ˘ ³˘)♥",
+        " ( ͡° ᴥ ͡°)",
+        " (^³^)",
+        " (´・ω・｀)",
+        " (ʘᗩʘ\")",
+        " (இωஇ )",
+        " (๑•́ ₃ •̀๑)",
+        " (• o •)",
+        " (•́︿•̀)",
+        " (⁎˃ᆺ˂)",
+        " (╯﹏╰）",
+        " (●´ω｀●)",
+        " (◠‿◠✿)",
+        " (✿ ♡‿♡)",
+        " (❁´◡`❁)",
+        " (　\"◟ \")",
+        " (人◕ω◕)",
+        " (；ω；)",
+        " (｀へ´)",
+        " ._.",
+        " :3",
+        " :3c",
+        " :D",
+        " :O",
+        " :P",
+        " ;-;",
+        " ;3",
+        " ;_;",
+        " <{^v^}>",
+        " >_<",
+        " >_>",
+        " UwU",
+        " XDDD",
+        " \\°○°/",
+        " ^-^",
+        " ^_^",
+        " ^•ﻌ•^",
+        " x3",
+        " x3",
+        " xD",
+        " ÙωÙ",
+        " ʕʘ‿ʘʔ",
+        " ʕ•ᴥ•ʔ",
+        " ミ(．．)ミ",
+        " ㅇㅅㅇ",
+        ", fwendo",
+        "（＾ｖ＾）",
+    ];
+    private readonly replacementMap = {
+        r: "w",
+        R: "W",
+        l: "w",
+        L: "W",
+        no: 'nu',
+        has: 'haz',
+        have: 'haz',
+        says: 'sez',
+        you: 'uu',
+        the: 'da'
+    };
+
+    public translateText(str: string): string {
+        return str.replace(/\b(?:r|R|l|L|no|has|have|says|you|the)/gi, matched => this.replacementMap[matched]);
+    };
+
+    public owo(str: string): string {
+        return `${this.prefixes[~(Math.random() * (this.prefixes.length - 1))]} ${this.translateText(str)} ${this.suffixes[~(Math.random() * (this.suffixes.length - 1))]}`;
+    }
+}
+  
+export default OwO;
