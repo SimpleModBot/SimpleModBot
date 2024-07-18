69940a271c61bd65324ebbd40b712c4c9a15e321
DEATHB4DEFEAT <zachcaffee@outlook.com>
2024-07-17 21:09:27 -0700

    fix ssArgsToServer

--- src/utils/handlers/ssArgsToServer.ts
+++ src/utils/handlers/ssArgsToServer.ts
@@ -2,19 +2,23 @@
 const servers = require("../json/servers.json");
 
 module.exports = function (args) {
+    // Defaults to the first server's IP
     let server = servers[0][1];
     const oldserver = server;
 
     if (args[0] !== 'ENA') {
         servers.forEach((srv) => {
+            // Check if the server is in the servers.json file
             if (srv[0] == args[0] && srv[1] !== server) {
+                // If the server is in the servers.json file, set the server to the server's IP
                 return (server = srv[1]);
             }
         });
-    }
 
-    if (args[0] !== 'ENA' && args[0].includes('.') && oldserver == server) {
-        server = args[0];
+        // Set the server to the content if it might be valid
+        if (oldserver == server) {
+            server = args[0];
+        }
     }
 
     return server;
